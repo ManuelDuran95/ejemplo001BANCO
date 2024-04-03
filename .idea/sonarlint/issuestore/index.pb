bd8706a77a8fced944d30a19cd04b6b6cad1aa4a
ManuelDuran95 <mata.duran@gmail.com>
2024-04-03 08:19:41 -0600

    crear repo

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+@
+build.gradle.kts,d\b\dbcff70658daf80b53ce624f6adcaa529df5ed8d
+S
+#src/main/java/com/apirest/Main.java,8\2\82f4dcd19a0391777dbb95f67bcba90b62824b30
---
+++ src/main/java/com/apirest/Main.java
@@ -0,0 +1,9 @@
+package com.apirest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hola mundo desde JAVA CON GRADLE");
+
+    }
+}
